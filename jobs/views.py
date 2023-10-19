51d51f472c45a0d7555f6adafed4aa68f56b026b
sam <sampurna.chapagainnn@gmail.com>
2023-10-19 12:14:16 +0100

    create new app-jobs

---
+++ jobs/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from .models import Job
+# Create your views here.
+
+def index(request):
+    jobs = Job.objects.all()
+
+    return render(request, 'jobs/index.html', {'jobs': jobs})
