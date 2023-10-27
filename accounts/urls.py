75547cfd3b93715bc6a99f20451b4839d27af788
sam <sampurna.chapagainnn@gmail.com>
2023-10-28 00:57:54 +0100

    add url for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
-# from .forms import SigninForm
+from .forms import SigninForm
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    # path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=SigninForm), name='login'),
+    path('signin/', auth_views.LoginView.as_view(template_name='accounts/signin.html', authentication_form=SigninForm), name='signin'),
     # path('logout/', views.logout_user, name='logout'),
    
 
