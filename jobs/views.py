0908ab0f461f7f76da6705e0cac4ae67d09a5b91
sam <sampurna.chapagainnn@gmail.com>
2023-10-27 17:26:26 +0100

    add view code for index view

--- jobs/views.py
+++ jobs/views.py
@@ -3,6 +3,9 @@ from .models import Job
 # Create your views here.
 
 def index(request):
-    jobs = Job.objects.all()
+    return render(request, 'jobs/index.html')
 
-    return render(request, 'jobs/index.html', {'jobs': jobs})
+
+def home(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs': jobs})
