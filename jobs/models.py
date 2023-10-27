606870cbce3529458f434cc95918f9548a4284d9
sam <sampurna.chapagainnn@gmail.com>
2023-10-27 17:27:12 +0100

    add companyname and url to jobs table

--- jobs/models.py
+++ jobs/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(max_length=50)
-    url = models.TextField
+    title = models.CharField(max_length=90)
+    companyname = models.CharField(max_length=90, default='')
+    url = models.CharField(max_length=90, default='')
 
     def __str__(self):
         return self.title
