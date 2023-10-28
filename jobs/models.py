bcdb7afc1a6f73aeb1284f80e4ec264c656109cb
sam <sampurna.chapagainnn@gmail.com>
2023-10-29 00:48:41 +0100

    add author as a foreign key to jobs table

--- jobs/models.py
+++ jobs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Job(models.Model):
     title = models.CharField(max_length=90)
+    author=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     companyname = models.CharField(max_length=90, default='')
     url = models.CharField(max_length=90, default='')
 
