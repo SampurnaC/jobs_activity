b27c905faa5a76dcf6a9550077deba8f1acae2f2
sam <sampurna.chapagainnn@gmail.com>
2023-10-31 23:01:50 +0000

    add view code for new method

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Job
-# Create your views here.
+from .forms import JobForm
 
 def index(request):
     return render(request, 'jobs/index.html')
@@ -9,3 +9,15 @@ def index(request):
 def home(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+def new(request):
+    if request.method == "POST":
+        form = JobForm(request.POST)
+        if form.is_valid():
+            form.instance.author=request.user
+            post=form.save()
+            post.save()
+            return redirect('/')
+    else:
+        form=JobForm()
+    return render(request, 'jobs/new.html', {'form': form})
