ce0b3e0e525014b6bd8cbcb30c35a6a660addc72
sam <sampurna.chapagainnn@gmail.com>
2023-11-01 09:24:04 +0000

    add status column to the jobs table

--- jobs/models.py
+++ jobs/models.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+STATUS_CHOICES = (
+    ('applied', 'APPLIED'),
+    ('interview', 'INTERVIEW'),
+    ('rejected', 'REJECTED'),
+    ('accepted', 'ACCEPTED'),
+
+)
 class Job(models.Model):
     title = models.CharField(max_length=90)
     author=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     companyname = models.CharField(max_length=90, default='')
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='applied')
     url = models.CharField(max_length=90, default='')
 
     def __str__(self):
