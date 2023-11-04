dabe0c6677acd6e6523366fc954f83ebc0df8f6b
sam <sampurna.chapagainnn@gmail.com>
2023-11-04 21:49:19 +0000

    refactor widgets for model form

--- jobs/forms.py
+++ jobs/forms.py
@@ -3,18 +3,26 @@ from .models import Job
 from django.contrib.auth.models import User
 
 class JobForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Enter your job title',
-        'class': 'form-control'
-    }))
-    companyname = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Enter the company name',
-        'class': 'form-control'
-    }))
-    url = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Enter job URL',
-        'class': 'form-control'
-    }))
     class Meta:
         model= Job
         fields=['title', 'companyname', 'url', 'status']
+        # title = forms.CharField(widget=forms.TextInput(attrs={
+        #     'placeholder': 'Enter your job title',
+        #     'class': 'form-control'
+        # }))
+        # companyname = forms.CharField(widget=forms.TextInput(attrs={
+        #     'placeholder': 'Enter the company name',
+        #     'class': 'form-control'
+        # }))
+        # url = forms.CharField(widget=forms.TextInput(attrs={
+        #     'placeholder': 'Enter job URL',
+        #     'class': 'form-control'
+        # }))
+        
+        widgets={
+            'title': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter your job title'}),
+            'companyname': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter the company name'}),
+            'url': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter job URL',}),
+            'status': forms.Select(attrs= {'class': 'form-control', 'style': 'appearance: auto; width: 200px;'})
+
+        }
