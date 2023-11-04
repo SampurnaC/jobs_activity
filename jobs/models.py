d1e3a2ead8ea006b3eb4e956d929ce4dc4429dc8
sam <sampurna.chapagainnn@gmail.com>
2023-11-04 21:50:06 +0000

    add status field to the database and change the max length of title, companyname, and url fields

--- jobs/models.py
+++ jobs/models.py
@@ -4,18 +4,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 STATUS_CHOICES = (
-    ('applied', 'APPLIED'),
-    ('interview', 'INTERVIEW'),
-    ('rejected', 'REJECTED'),
-    ('accepted', 'ACCEPTED'),
+    ('Applied', 'APPLIED'),
+    ('Interview', 'INTERVIEW'),
+    ('Rejected', 'REJECTED'),
+    ('Accepted', 'ACCEPTED'),
 
 )
 class Job(models.Model):
-    title = models.CharField(max_length=90)
+    title = models.CharField(max_length=50)
     author=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    companyname = models.CharField(max_length=90, default='')
+    companyname = models.CharField(max_length=50, default='')
     status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='applied')
-    url = models.CharField(max_length=90, default='')
+    url = models.CharField(max_length=80, default='')
 
     def __str__(self):
         return self.title
