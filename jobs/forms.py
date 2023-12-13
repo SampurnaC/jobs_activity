7df983c057bad81da7dc8821fe7e9d13183c18ed
sam <sampurna.chapagainnn@gmail.com>
2023-12-13 19:12:44 +0000

    add textarea widget for extra note in jobs form

--- jobs/forms.py
+++ jobs/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class JobForm(forms.ModelForm):
     class Meta:
         model= Job
-        fields=['title', 'companyname', 'url', 'status']
+        fields=['title', 'companyname', 'url', 'status', 'extra_note']
         # title = forms.CharField(widget=forms.TextInput(attrs={
         #     'placeholder': 'Enter your job title',
         #     'class': 'form-control'
@@ -23,6 +23,7 @@ class JobForm(forms.ModelForm):
             'title': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter your job title'}),
             'companyname': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter the company name'}),
             'url': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter job URL',}),
-            'status': forms.Select(attrs= {'class': 'form-control', 'style': 'appearance: auto; width: 200px;'})
+            'status': forms.Select(attrs= {'class': 'form-control', 'style': 'appearance: auto; width: 200px;'}),
 
+            'extra_note': forms.Textarea(attrs= {'class': 'form-control', 'rows': 6, 'cols': 15, 'placeholder': 'Add extra note here. Leave it blank if there is nothing.',}),
         }
