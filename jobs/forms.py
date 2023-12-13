3de47583d1c6c9e8511d1777e32f36180af59b7c
sam <sampurna.chapagainnn@gmail.com>
2023-12-13 22:21:34 +0000

    remove commented code

--- jobs/forms.py
+++ jobs/forms.py
@@ -6,19 +6,6 @@ class JobForm(forms.ModelForm):
     class Meta:
         model= Job
         fields=['title', 'companyname', 'url', 'status', 'extra_note']
-        # title = forms.CharField(widget=forms.TextInput(attrs={
-        #     'placeholder': 'Enter your job title',
-        #     'class': 'form-control'
-        # }))
-        # companyname = forms.CharField(widget=forms.TextInput(attrs={
-        #     'placeholder': 'Enter the company name',
-        #     'class': 'form-control'
-        # }))
-        # url = forms.CharField(widget=forms.TextInput(attrs={
-        #     'placeholder': 'Enter job URL',
-        #     'class': 'form-control'
-        # }))
-        
         widgets={
             'title': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter your job title'}),
             'companyname': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter the company name'}),
