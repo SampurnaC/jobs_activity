abafb06c48bddb216d470238de05f3ec007bf829
sam <sampurna.chapagainnn@gmail.com>
2023-12-13 05:55:01 +0000

    add created_at and updated_at fields

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 STATUS_CHOICES = (
@@ -16,6 +17,8 @@ class Job(models.Model):
     companyname = models.CharField(max_length=50, default='')
     status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='applied')
     url = models.TextField(max_length=800, default='')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.title
