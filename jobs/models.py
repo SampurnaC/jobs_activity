c93fe522f680b00c6ff6867453e1433f852234ed
sam <sampurna.chapagainnn@gmail.com>
2023-12-13 22:22:20 +0000

    increase max_length for CharFields in jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -12,10 +12,10 @@ STATUS_CHOICES = (
 
 )
 class Job(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=80)
     author=models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    companyname = models.CharField(max_length=50, default='')
-    status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='applied')
+    companyname = models.CharField(max_length=80, default='')
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='applied')
     url = models.TextField(max_length=800, default='')
     extra_note = models.TextField(max_length=800, default='', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
