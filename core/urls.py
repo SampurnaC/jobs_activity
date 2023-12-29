c1dc27014d76e5ad0cdc41a854bb2032764ba957
sam <sampurna.chapagainnn@gmail.com>
2023-12-29 12:52:23 +0000

    add url for robots.txt and sitemap

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.contrib.sitemaps.views import sitemap
+
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {'static': StaticViewSitemap}
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name='jobs/robots.txt', content_type='text/plain')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('admin/', admin.site.urls),
     path('', include('jobs.urls')),
     path('users/', include('accounts.urls')),
