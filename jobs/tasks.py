ac2e166b1eb43ecb032f015834c5ab2d0260ef5e
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 19:31:35 +0000

    async script to export jobs to csv

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -1,36 +1,16 @@
-from .models import Job, Yo
+from .models import Job
 from django.http import HttpResponse
+import os
 import csv
 import pandas as pd
 from celery import shared_task
 from django.conf import settings
-# @shared_task(bind=True)
-# def test_func(self):
-#     for i in range(10):
-#         print(i)
-#     return "Coming from tasks.py"
 
 @shared_task(bind=True)
 def export(request, user_id):
     
-    # current_user=request.user
-    # jobs = Job.objects.filter(author_id=current_user.id).order_by('-created_at')
-
-
-    # jobs=Yo.objects.all()
-    # response = HttpResponse('text/csv')
-    # response['Content-Disposition'] = 'attachment; filename=jobs_export.csv'
-    # writer = csv.writer(response)
-    # writer.writerow(['Name'])
-    # jobs_fields = jobs.values_list('name')
-    # for job in jobs_fields:
-    #     writer.writerow(job)
-    # return response
-    # current_user=request.user.id
     objs = Job.objects.filter(author_id=user_id).order_by('-created_at')
-    # objs=Yo.objects.all()
     payload = []
-
     for obj in objs:
         payload.append({
             'Position': obj.title,
@@ -40,4 +20,14 @@ def export(request, user_id):
 
         })
     df = pd.DataFrame(payload)
-    df.to_csv(f"yoman1", encoding='UTF-8')
+
+    # Get the downloads folder path
+    downloads_folder = os.path.expanduser("~/Downloads")
+    
+    # Define the file path
+    file_path = os.path.join(downloads_folder, 'AllJobs.csv')
+
+    # Export DataFrame to CSV
+    df.to_csv(file_path, index=False)
+
+    # df.to_csv(f"yoman1", encoding='UTF-8')
