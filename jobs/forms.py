b52e1480438bef6f577960bfa75ef1edcf965515
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 19:29:07 +0000

    add category dropdown in jobs-forms

--- jobs/forms.py
+++ jobs/forms.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class JobForm(forms.ModelForm):
     class Meta:
         model= Job
-        fields=['title', 'companyname', 'url', 'status', 'extra_note']
+        fields=['title', 'companyname', 'url', 'category', 'status', 'extra_note']
         widgets={
             'title': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter your job title'}),
             'companyname': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter the company name'}),
             'url': forms.TextInput(attrs= {'class': 'form-control', 'placeholder': 'Enter job URL',}),
+            'category': forms.Select(attrs= {'class': 'form-control', 'style': 'appearance: auto; width: 200px;'}),
             'status': forms.Select(attrs= {'class': 'form-control', 'style': 'appearance: auto; width: 200px;'}),
-
             'extra_note': forms.Textarea(attrs= {'class': 'form-control', 'rows': 6, 'cols': 15, 'placeholder': 'Add extra note here. Leave it blank if there is nothing.',}),
         }
