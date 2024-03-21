309719c938f00ea2c126c449dde8bd6e7e59128b
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 19:31:02 +0000

    route for jobs category and export

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('asynctasks/', views.test, name='test'),
-
     path('', views.index, name='index'),
     path('jobs/', views.home, name='home'),
     path('jobs/new/', views.new, name='new'),
     path('jobs/edit/<int:id>/', views.edit, name='edit'),
     path('jobs/delete/<int:id>', views.delete, name='delete'),
     path('jobs/<int:id>', views.show_job, name='show'),
-    path('jobs/search_job', views.search_job, name='search-job'),
+    path('jobs/category/<int:id>', views.category_jobs, name='category-jobs'),
+    
+    path('export/', views.export_to_csv, name='export'),
 
 ]
