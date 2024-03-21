4e92ec5783cefbf8e06692d504409af341c4e0c4
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 08:28:07 +0000

    add views for signup, sign in and sign out

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from accounts.forms import SignupForm
-from django.contrib.auth import logout
+from accounts.forms import SignupForm, SigninForm
+from django.contrib.auth import get_user_model, login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -8,12 +11,48 @@ def signup(request):
     if request.method == "POST":
         form=SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
+            messages.success(request, f"New account created {user.username}")
             return redirect('/users/signin')
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
     else:
         form=SignupForm()    
     return render(request, 'accounts/signup.html', {'form': form})
 
-def logout_user(request):
+@login_required
+def custom_logout(request):
     logout(request)
+    messages.info(request, "Logged out successfully!")
     return redirect('/')
+
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        form = SigninForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
+                return redirect('/')
+
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error) 
+
+    form = SigninForm() 
+    
+    return render(
+        request=request,
+        template_name="accounts/signin.html", 
+        context={'form': form}
+        )
