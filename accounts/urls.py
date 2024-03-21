a52873948e26f636dd8750a4bfe92cb9d18dd221
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 08:28:36 +0000

    add custom routes for signin, and sign out

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,11 @@ from .forms import SigninForm
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('signin/', auth_views.LoginView.as_view(template_name='accounts/signin.html', authentication_form=SigninForm), name='signin'),
-    path('logout/', views.logout_user, name='logout'),
+    # path('signin/', auth_views.LoginView.as_view(template_name='accounts/signin.html', authentication_form=SigninForm), name='signin'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='accounts/signout.html'), name='logout'),
+    path('signin/', views.custom_login, name='signin'),
+    path('logout/', views.custom_logout, name='logout'),
+
    
 
 
