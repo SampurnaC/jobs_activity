d57496914e2d23560bea5dcaf7cd9736812dda80
sam <sampurna.chapagainnn@gmail.com>
2024-03-21 19:28:19 +0000

    add function for activate email and activate

--- accounts/views.py
+++ accounts/views.py
@@ -5,17 +5,64 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
+from .tokens import account_activation_token
+
+
+def activateEmail(request, user, to_email):
+    mail_subject = 'Activate your user account.'
+    message = render_to_string('accounts/template_activate_account.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
+            received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending confirmation email to {to_email}, check if you typed it correctly.')
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, 'Thank you for your email confirmation. Now you can login your account.')
+        return redirect('signin')
+    else:
+        messages.error(request, 'Activation link is invalid!')
+    
+    return redirect('/')
 
 def signup(request):
     if request.method == "POST":
         form=SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, f"New account created {user.username}")
+            user = form.save(commit=False)
+            user.is_active=False
+            user.save()
+            activateEmail(request,user,form.cleaned_data.get('email'))
+            # login(request, user)
+            # messages.success(request, f"New account created {user.username}")
             return redirect('/users/signin')
         else:
+            breakpoint()
             for error in list(form.errors.values()):
                 messages.error(request, error)
     else:
