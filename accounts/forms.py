c111f0ec6e79dbea45450428ee2b6d2aa228ce88
sam <sampurna.chapagainnn@gmail.com>
2024-03-24 20:29:25 +0000

    add model form to update profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -41,3 +41,10 @@ class SigninForm(AuthenticationForm):
         'placeholder': 'Your password',
         'class': 'form-control'
     }))
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email', 'description']
